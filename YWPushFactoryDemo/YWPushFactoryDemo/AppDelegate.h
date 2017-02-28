6cbb5df6de41bbfaaf043b4bf61220589281a738
yw <yw@ywdeMac-mini.local>
2017-02-28 19:59:20 +0800

    初始化项目

---
+++ YWPushFactoryDemo/YWPushFactoryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YWPushFactoryDemo
+//
+//  Created by yw on 2017/2/28.
+//  Copyright © 2017年 yw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
